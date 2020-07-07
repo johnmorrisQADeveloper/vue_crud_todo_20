e85e01b9f8eb2b956d9f15b6aefbc8d79736ae73
john morris <fennymorris@live.co.uk>
2020-07-07 22:04:42 +0100

    change to heroku api

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -11,28 +11,28 @@ const getters = {
 
 const actions = {
   async fetchTodos ({ commit }) {
-    const response = await axios.get('http://localhost:3000/todos')
+    const response = await axios.get('https://cryptic-sierra-47326.herokuapp.com/todos')
     commit('SET_TODOS', response.data)
   },
   async addTodo ({ commit }, title) {
-    const response = await axios.post('http://localhost:3000/todos', {
+    const response = await axios.post('https://cryptic-sierra-47326.herokuapp.com/todos', {
       title, completed: false
     })
     commit('NEW_TODO', response.data)
   },
   async deleteTodo ({ commit }, _id) {
-    await axios.delete(`http://localhost:3000/todos/${_id}`)
+    await axios.delete(`https://cryptic-sierra-47326.herokuapp.com/todos/${_id}`)
     commit('REMOVE_TODO', _id)
   },
   async filterTodo ({ commit }, number) {
-    const response = await axios.get(`http://localhost:3000/todos/count/${number}`)
+    const response = await axios.get(`https://cryptic-sierra-47326.herokuapp.com/todos/count/${number}`)
     commit('SET_TODOS', response.data)
   },
   async updateTodo ({ commit }, updateTodo) {
     console.log(JSON.stringify(updateTodo))
-    const response = await axios.patch(`http://localhost:3000/todos/${updateTodo._id}`, { ...updateTodo })
+    const response = await axios.patch(`https://cryptic-sierra-47326.herokuapp.com/todos/${updateTodo._id}`, { ...updateTodo })
     commit('UPDATE_TODOS', response.data)
-    const responses = await axios.get('http://localhost:3000/todos')
+    const responses = await axios.get('https://cryptic-sierra-47326.herokuapp.com/todos')
     commit('SET_TODOS', responses.data)
   }
 }
